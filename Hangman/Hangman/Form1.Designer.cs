eb9980047cdb1b9cfe4c1cc746599dd427ccccdc
roderik10 <roderik182@gmail.com>
2014-08-25 22:19:10 -0600

    second

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Probar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Registrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Top = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Probar
+            // 
+            this.Probar.Location = new System.Drawing.Point(319, 113);
+            this.Probar.Name = "Probar";
+            this.Probar.Size = new System.Drawing.Size(75, 24);
+            this.Probar.TabIndex = 0;
+            this.Probar.Text = "Probar";
+            this.Probar.UseVisualStyleBackColor = true;
+            this.Probar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(18, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Location = new System.Drawing.Point(112, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Resultado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(598, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Intentos disponibles";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(127, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ingrese su nombre";
+            // 
+            // Registrar
+            // 
+            this.Registrar.Location = new System.Drawing.Point(33, 75);
+            this.Registrar.Name = "Registrar";
+            this.Registrar.Size = new System.Drawing.Size(75, 24);
+            this.Registrar.TabIndex = 10;
+            this.Registrar.Text = "Registrar";
+            this.Registrar.UseVisualStyleBackColor = true;
+            this.Registrar.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(424, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 282);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Top
+            // 
+            this.Top.Location = new System.Drawing.Point(511, 479);
+            this.Top.Name = "Top";
+            this.Top.Size = new System.Drawing.Size(75, 24);
+            this.Top.TabIndex = 12;
+            this.Top.Text = "Top 10";
+            this.Top.UseVisualStyleBackColor = true;
+            this.Top.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox2.Location = new System.Drawing.Point(48, 271);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(252, 220);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(637, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "__";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 494);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "MUERTO :S";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(121, 494);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "CRACK! ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 545);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Top);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Registrar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Probar);
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Probar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Registrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Top;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+
+    //Clase cliente del juego
+    public partial class Form1 : Form
+    {
+
+        private titanic.B02430_Hangman ws;  //instancia del servicio
+        private System.Net.CookieContainer cookies; //cookies para el servidor
+        private String answer;  //Respuesta de una partida
+        private String hidden;  //String con la forma _ _ a _ _ que se va formando conforme se forma la respuesta
+        private String usuario; //Jugador de la partida
+        private String[,] topTen;   //Matriz para cargar la consulta de mejores tiempos desde la BD
+        private String[] rows;  //Array auxiliar para cargar la consulta
+        private int cont;   //contador para cargar las imágenes
+        private int intentos;   //cantidad de intentos hecho por jugador
+
+        /* 
+        * Contructor de la clase
+        */
+        public Form1()
+        {
+            InitializeComponent();
+
+            cont = 1;
+        }
+
+        /* 
+        * Método que carga la ventana 
+        */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("http://titanic.ecci.ucr.ac.cr/~eb02430/TP2/hang/hang0.gif");  //método que carga la imagen inicial del ahorcado
+            ws = new Hangman.titanic.B02430_Hangman();  //creación del servicio
+            cookies = new System.Net.CookieContainer(); //creación del cookie
+            ws.CookieContainer = cookies;   //asignación al servicio
+            answer = ws.fetchWordArray("palabras.txt"); //escoge la palabra para el juego
+            hidden = ws.hideCharacters(answer, answer.Length);  //esconde la palabra con _ _ _
+            colocarResultado(hidden);   //coloca la palabra escondida en pantalla
+            intentos = ws.getIntentos();    
+            label3.Text = intentos.ToString();  //coloca los intentos disponibles en pantalla
+
+            /* Deshabilita botones y campos de texto */
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            Top.Enabled = false;
+            Probar.Enabled = false;
+            label5.Visible = false;
+            label6.Visible = false;
+        }
+
+
+        /* 
+         * Método al presionar el botón de Probar 
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String letraIngresada = " ";
+
+            if(textBox2.Text.ToString()!=""){   //si el usuario no ingresa nada y oprime el botón, pierde un turno
+                letraIngresada = textBox2.Text.ToString();
+            }
+
+            hidden = ws.checkAndReplace(letraIngresada, hidden, answer, answer.Length);   //revisa y reemplaza (si hubo acierto) el intento del jugador
+            textBox2.Text = "";
+
+            
+            if(ws.checkGameOver(answer, hidden)==0){    //Juego perdido
+
+                /* deshabilita botones y campos de texto */
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                Probar.Enabled = false;
+
+                pictureBox1.Load("http://titanic.ecci.ucr.ac.cr/~eb02430/TP2/hang/hang10.gif"); 
+                pictureBox2.Load("http://titanic.ecci.ucr.ac.cr/~eb02430/TP2/hang/hangFinal.jpg");  //carga imagen de pérdida
+
+                rows = ws.select(); //select del top ten de mejores tiempos
+                topTen = new String[rows.Length, 2];
+                Top.Enabled = true; //se habilita la opción de mostrar el top ten
+                intentos--;
+                label5.Visible = true;
+                label5.Text += "  Respuesta: " + answer;
+            
+            }else{
+
+                colocarResultado(hidden);   //coloca palabra escondida en pantalla
+
+                if (ws.checkGameOver(answer, hidden) == 1){     //Juego ganado
+
+                    /* deshabilita botones y campos de texto */
+                    textBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    Probar.Enabled = false;
+
+                    ws.insert(usuario, ws.getTime());   //inserta el jugador y tiempo en la BD
+
+                    pictureBox2.Load("http://titanic.ecci.ucr.ac.cr/~eb02430/TP2/hang/like.jpg");   //carga imagen ganadora
+
+                    /* select a la BD y habilitación para ver el top ten */
+                    rows = ws.select();
+                    topTen = new String[rows.Length, 2];
+                    Top.Enabled = true;
+                    label6.Visible = true;
+                }
+                else {
+                    if (ws.checkGameOver(answer, hidden) == -1) {   //Intento fallido
+                        pictureBox1.Load("http://titanic.ecci.ucr.ac.cr/~eb02430/TP2/hang/hang"+cont+".gif");   //se carga la imagen formando el ahorcado
+                        cont++; //para cargar la siguiente imagen
+                        intentos--; //un intento menos por fallar                                        
+                    }
+                }               
+            }
+
+            label3.Text = intentos.ToString();  //actualización de los intentos en pantalla
+        }
+
+
+        /* Método auxiliar para cargar el resultado de la consulta a la BD. 
+         * El resultado viene en un string. Se separa por columnas y se guarda para luego cargar en pantalla.
+         */
+        private void splitRows() {
+
+            for(int i=0;i<rows.Length;i++){
+
+                String[] x = rows[i].Split('-');
+
+                for(int j=0;j<2;j++){
+
+                    topTen[i, j] = x[j];
+                }
+            }
+        }
+
+        /*
+         * Coloca el resultado parcial en pantalla
+         */
+        private void colocarResultado(String s) {
+
+            textBox1.Text = "               ";
+            for (int i = 0; i < s.Length; i++) {
+                textBox1.Text += "  "+s[i];                
+            }
+        }
+       
+
+
+
+        /* 
+         * Método que registra un jugador.
+         */ 
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            colocarResultado(hidden);
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            Probar.Enabled = true;
+            textBox3.Enabled = false;
+            Registrar.Enabled = false;
+
+            usuario = textBox3.Text.ToString();
+
+        }
+
+
+        /*
+         * Muestra el top ten de mejores tiempos
+         */
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Top.Enabled = false;
+            splitRows();
+            DataTable table = new DataTable();
+
+            /* Agrega columnas en el data table */
+            table.Columns.Add("Jugador");
+            table.Columns.Add("Tiempo en segundos");
+
+            // Para cada columna
+            for (int i = 0; i < 2; i++)
+            {
+                //Lista de objects
+                List<object> objectRows = new List<object>();
+
+                //Convierte las filas para la columna en objects
+                for (int k = 0; k < rows.Length; k++) {
+                    objectRows.Add((object)topTen[k, i]);
+                }
+
+
+                // Agrega las filas (visuales) en el data table
+                while (table.Rows.Count < objectRows.Count)
+                {
+                    table.Rows.Add();
+                }
+
+                // Agrega los datos de las filas en el data table
+                for (int a = 0; a < objectRows.Count; a++)
+                {
+                    table.Rows[a][i] = objectRows[a];
+                }
+            }
+
+            dataGridView1.DataSource = table; //Agrega el data table en el datagrid
+            
+        }
+
+    }
+}
